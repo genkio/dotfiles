9ed0fe02a704850c6e011cb1efecd363da3e77f7
genkio <5327840+genkio@users.noreply.github.com>
2026-04-07 10:14:13 +0900

    fix(nvim): deprecate vim.tbl_flatten

--- nvim/.config/nvim/lua/custom/plugins/colorizer.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorizer.lua
@@ -5,6 +5,22 @@ return {
   'norcalli/nvim-colorizer.lua',
   event = { 'BufReadPre', 'BufNewFile' },
   config = function()
-    require('colorizer').setup()
+    local colorizer = require 'colorizer'
+    local group = vim.api.nvim_create_augroup('UserColorizer', { clear = true })
+
+    -- Avoid the plugin's deprecated setup() path and attach directly.
+    vim.api.nvim_create_autocmd('FileType', {
+      group = group,
+      callback = function(args)
+        if vim.bo[args.buf].buftype == '' then
+          colorizer.attach_to_buffer(args.buf)
+        end
+      end,
+    })
+
+    local current_buf = vim.api.nvim_get_current_buf()
+    if vim.bo[current_buf].filetype ~= '' and vim.bo[current_buf].buftype == '' then
+      colorizer.attach_to_buffer(current_buf)
+    end
   end,
 }

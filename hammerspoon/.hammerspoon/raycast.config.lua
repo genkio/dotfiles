f07325de4a72719a5766d0828572e1da22357523
genkio <5327840+genkio@users.noreply.github.com>
2026-05-11 11:43:46 +0900

    update(hammerspoon): tweak quick links

--- hammerspoon/.hammerspoon/raycast.config.lua
+++ hammerspoon/.hammerspoon/raycast.config.lua
@@ -1,41 +1,32 @@
 return {
   quickLinks = {
     {
-      name = "Search Google",
-      link = "https://google.com/search?q={query}",
-    },
-    {
-      name = "Search DuckDuckGo",
-      link = "https://duckduckgo.com/?q={query}",
-    },
-    {
       name = "Ask Google",
       link = "https://google.com/search?q={query}&udm=50",
-    },
-    {
-      name = "Ask Grok",
-      link = "https://grok.com/?q={query}",
     },
     {
       name = "Ask ChatGPT",
       link = "https://chatgpt.com/?q={query}&temporary-chat=true&model=gpt-5-instant",
     },
     {
+      name = "Ask Grok",
+      link = "https://grok.com/?q={query}",
+    },
+    {
       name = "Ask All",
       links = {
-        "https://google.com/search?q={query}&udm=50",
         "https://grok.com/?q={query}",
         "https://chatgpt.com/?q={query}&temporary-chat=true&model=gpt-5-instant",
+        "https://google.com/search?q={query}&udm=50",
       },
     },
     {
-      name = "Goto Github",
-      link = "https://github.com/Cryptact/cryptact/pull/{query}",
+      name = "Search Google",
+      link = "https://google.com/search?q={query}",
     },
     {
-      iconName = "computer-chip-16",
-      name = "Goto YouTrack",
-      link = "https://cryptact.myjetbrains.com/youtrack/issue/GRID-{query}",
+      name = "Search DuckDuckGo",
+      link = "https://duckduckgo.com/?q={query}",
     },
   },
 }

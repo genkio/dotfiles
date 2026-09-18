d50ec4f004d380600a63975786f72c19f6880f82
genkio <5327840+genkio@users.noreply.github.com>
2026-09-18 16:11:27 +0900

    update(keybind): quick launch preview

--- hammerspoon/.hammerspoon/rcmd.config.lua
+++ hammerspoon/.hammerspoon/rcmd.config.lua
@@ -19,6 +19,7 @@ return {
   m = "Mail",
   n = { action = "notification_center" },
   o = { action = "finder_in_alacritty" },
+  p = "Preview",
   q = { action = "run_in_alacritty", command = "vi ~/box/notes.txt" },
   t = "TablePlus",
   s = "Sublime Text",
